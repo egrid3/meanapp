7e54a894f7956f89c6129c94f7294e9fd2d3d01a
egrid3 <egridley@ieee.org>
2019-08-05 19:33:20 -0400

    angular add-on

--- meanapp.js
+++ meanapp.js
@@ -44,6 +44,9 @@ app.get('/', (req, res) => {
   res.send('Somethin\' got jacked up! - Invalid Endpoint');
 });
 
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public/index.html'));
+});
 // start server
 app.listen(port, () => {
   console.log('All engines are a go on port: '+port+'!');
